7385d05d40edf0151b9e6fe3d77dbca05633fc66
Duy <duynguyenquykhanh@gmail.com>
2018-03-08 14:19:38 -0800

    change Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,7 +1,9 @@
 
 public class Palindrome
 {
-    
+    public boolean isPalindrome(String word){
+        
+    }
     public Deque<Character> wordToDeque(String word){
         Deque<Character> res = new LinkedListDeque<Character>();
         for (int i = 0; i < word.length(); i++){
